ef40231297ebc97e8a056a8e8ea747dbc84eda3c
Zapera <contato@zapera.com.br>
2026-07-03 23:01:46 -0300

    feat(white-label): allow profile settings menu

--- app/javascript/dashboard/components-next/sidebar/SidebarProfileMenu.vue
+++ app/javascript/dashboard/components-next/sidebar/SidebarProfileMenu.vue
@@ -73,7 +73,7 @@ const menuItems = computed(() => {
       },
     },
     {
-      show: showUserMenuItems.value,
+      show: true,
       showOnCustomBrandedInstance: true,
       label: t('SIDEBAR_ITEMS.PROFILE_SETTINGS'),
       icon: 'i-lucide-user-pen',
