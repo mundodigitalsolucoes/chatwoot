00171c7a3151645b2e7b7cd4deca602ca3a9bb95
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-09-07 20:53:03 -0300

    feat(mds): restore white-label feature registry on v4.17.0

---
+++ app/javascript/dashboard/helper/mdsWhiteLabelFeatures.js
@@ -0,0 +1,40 @@
+import { isMdsWhiteLabelModeEnabled } from './mdsWhiteLabel';
+
+export const MDS_WHITE_LABEL_FEATURES = Object.freeze({
+  FEATURES_ARCHITECTURE: 'featuresArchitecture',
+  HIDE_CAPTAIN_MENU: 'hideCaptainMenu',
+  HIDE_HELP_CENTER_MENU: 'hideHelpCenterMenu',
+  HIDE_CAMPAIGNS_MENU: 'hideCampaignsMenu',
+  HIDE_SETTINGS_MENU: 'hideSettingsMenu',
+  HIDE_USER_MENU_ITEMS: 'hideUserMenuItems',
+  HIDE_PROFILE_SECURITY: 'hideProfileSecurity',
+  HIDE_CHATWOOT_LOGO: 'hideChatwootLogo',
+  HIDE_UPDATE_BANNER: 'hideUpdateBanner',
+  HIDE_CHATWOOT_METADATA: 'hideChatwootMetadata',
+});
+
+const MDS_WHITE_LABEL_FEATURE_CONFIG = Object.freeze({
+  [MDS_WHITE_LABEL_FEATURES.FEATURES_ARCHITECTURE]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_CAPTAIN_MENU]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_HELP_CENTER_MENU]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_CAMPAIGNS_MENU]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_SETTINGS_MENU]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_USER_MENU_ITEMS]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_PROFILE_SECURITY]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_CHATWOOT_LOGO]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_UPDATE_BANNER]: true,
+  [MDS_WHITE_LABEL_FEATURES.HIDE_CHATWOOT_METADATA]: true,
+});
+
+export const getMdsWhiteLabelFeatures = () => {
+  if (!isMdsWhiteLabelModeEnabled()) {
+    return {};
+  }
+
+  return MDS_WHITE_LABEL_FEATURE_CONFIG;
+};
+
+export const mdsWhiteLabelFeatures = getMdsWhiteLabelFeatures;
+
+export const isMdsWhiteLabelFeatureEnabled = featureName =>
+  Boolean(mdsWhiteLabelFeatures()[featureName]);
